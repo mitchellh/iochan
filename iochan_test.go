e9f5309a30619ffbada4c2e863fc028ef5803288
Adrien Delorme <adrien.delorme@icloud.com>
2019-04-08 11:43:11 +0200

    add LineReader func that uses bufio.NewScanner

--- iochan_test.go
+++ iochan_test.go
@@ -3,6 +3,7 @@ package iochan
 import (
 	"bytes"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -20,5 +21,25 @@ func TestDelimReader(t *testing.T) {
 
 	if !reflect.DeepEqual(results, expected) {
 		t.Fatalf("unexpected results: %#v", results)
+	}
+}
+
+func TestLineReader(t *testing.T) {
+
+	data := []string{"foo", "bar", "baz"}
+
+	buf := new(bytes.Buffer)
+	buf.WriteString(strings.Join(data, "\n") + "\n")
+
+	ch := LineReader(buf)
+
+	var result []string
+	expected := data
+	for v := range ch {
+		result = append(result, v)
+	}
+
+	if !reflect.DeepEqual(result, expected) {
+		t.Fatalf("unexpected results: %#v", result)
 	}
 }
